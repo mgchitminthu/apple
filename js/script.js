a50cd4d9669a32bc9dc0ecd40377150e1c105c4b
Chit Min Thu <chitminthu660081196@gmail.com>
2024-12-23 21:15:12 +0630

    four commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-let myFunction = ()=> {
-    let ulContainer = document.querySelector("#ulContainer");
-    if(ulContainer.style.display === "none") {
-        ulContainer.style.display = "inline";
-    } else {
-        ulContainer.style.display = "none";
-    }
+let myFunction = () => {
+  let ulContainer = document.querySelector("#ulContainer");
+  if (ulContainer.style.display === "none") {
+    ulContainer.style.display = "inline";
+  } else {
+    ulContainer.style.display = "none";
+  }
+};
+// carousel
+document.querySelector("#prev").onclick = prev;
+document.querySelector("#next").onclick = next;
+setInterval(next, 3000);
+let img = 1;
+function prev() {
+  document.querySelector("#img" + img).style.display = "none";
+  img--;
+  if (img === 0) img = 5;
+  document.querySelector("#img" + img).style.display = "inline";
+}
+
+function next() {
+  document.querySelector("#img" + img).style.display = "none";
+  img++;
+  if (img > 5) img = 1;
+  document.querySelector("#img" + img).style.display = "inline";
 }
